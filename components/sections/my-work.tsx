603cbfa62e0181dd88d10460806a0c8cc04074dd
Robspin <robin@tracefy.com>
2023-08-07 09:16:19 +0200

    progress

--- components/sections/my-work.tsx
+++ components/sections/my-work.tsx
@@ -4,8 +4,17 @@ import { PageProps } from "@/utils/types"
 
 const MyWork = ({ navigateTo }: PageProps) => {
     return (
-        <PageContainer className="translate-x-[-100vw]">
+        <PageContainer className="translate-x-[-100vw] px-24 py-16">
             <NavigationButton onClick={() => navigateTo('aboutMe')} text="my work" className="button-vertical-right" vertical />
+            <div className="w-full h-full flex gap-10">
+                <div className="w-[200px] shrink-0 flex flex-col gap-10 overflow-y-auto">
+                    <div className="w-[200px] h-[200px] shrink-0 bg-red-400"></div>
+                    <div className="w-[200px] h-[200px] shrink-0 bg-red-400"></div>
+                    <div className="w-[200px] h-[200px] shrink-0 bg-red-400"></div>
+                    <div className="w-[200px] h-[200px] shrink-0 bg-red-400"></div>
+                </div>
+                <div className="w-full h-full bg-green-300"></div>
+            </div>
         </PageContainer>
     )
 }
