9cea30ced70968837e378f4c080db0b2a37ca57b
Robspin <robin@tracefy.com>
2023-10-12 18:11:00 +0200

    updated bio and timeline

--- components/bio.tsx
+++ components/bio.tsx
@@ -8,6 +8,7 @@ export default function Bio() {
                         Let me share a bit about myself.<br/>
                         A while ago I used to fix cars for a living.<br/>
                         Now I am a Javascript fanboy who loves tinkering in frontend code.<br/>
+                        Some technologies which I am passionate about are react, react-native, threejs and deno<br/>
                         <br/>
                         For the past 2,5 years I have been working at Tracefy, a late stage startup focussing on gps solutions and preventing e-bike theft.<br/>
                         Because we had a small team I had a wide variety of responsibilities.<br/>
@@ -18,10 +19,10 @@ export default function Bio() {
                         - Helping to design the new micro-architecture landscape<br/>
                         - Drawing out our existing micro-architecture to create stability<br/>
                         - Designing the new Tracefy app.<br/>
-                        - Building the new Tracefy app leading a team with two external developers<br/>
+                        - Leading the creation of the new Tracefy app along with two external developers<br/>
                         - Teaching react and react-native to interns and external developers<br/>
                         <br/>
-                        The last few years where quite the ride to say the least.<br/>
+                        If you wish to see some of my past projects head over to my work.<br/>
                         <br/>
                     </p>
                 </div>
--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,82 +1,69 @@
+type TimelineItemProps = {
+    title: string
+    timeframe: string
+    description: string
+}
+
+const TimelineItem = ({ title, timeframe, description }: TimelineItemProps) => (
+    <li className="mb-10 ml-6">
+        <span
+            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
+                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{title}
+        </h3>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{timeframe}
+        </time>
+        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{description}</p>
+        {/*<a href="#"*/}
+        {/*   className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">*/}
+        {/*    <svg className="w-3.5 h-3.5 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"*/}
+        {/*         fill="currentColor" viewBox="0 0 20 20">*/}
+        {/*        <path*/}
+        {/*            d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>*/}
+        {/*        <path*/}
+        {/*            d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>*/}
+        {/*    </svg>*/}
+        {/*    Download ZIP</a>*/}
+    </li>
+)
+
+
+
+const timelineData: TimelineItemProps[] = [
+    {
+        title: "Technical Specialist at Rijnwoud, working on Skoda & Seat cars",
+        timeframe: '2009 - 2018',
+        description: 'From a rookie car mechanic to a certified Technical Specialist I learned how to find and fix mechanical and electronic issues'
+    },
+    {
+        title: "Traveling and studying Japanese",
+        timeframe: '2018 - March 2020',
+        description: 'After quiting my previous job I decided to travel for a while. Eventually living in Japan for 14 months. And attending a language school for 9 months.'
+    },
+    {
+        title: "Learning front-end development & freelancing",
+        timeframe: 'March 2020 - June 2021',
+        description: 'Having attained conversational level Japanese, this gave me the confidence to pursue a new career path. I started learning front-end development from scratch. And after learning the basics moving to react. My goal was speedrun the path of becoming a developer. Looking back, I succeeded.'
+    },
+    {
+        title: "Front-end developer Tracefy",
+        timeframe: 'June 2021 - Now',
+        description: 'After building a decent portfolio containing past freelance projects. I got hired by Tracefy. Where I\'m still working today.'
+    },
+]
 
 
 export default function Timeline() {
-
-    return <div className="text-white lg:w-[1024px] overflow-hidden pb-24 pl-8">
-
-        <ol className="relative border-l border-gray-200 dark:border-gray-700">
-            <li className="mb-10 ml-6">
-        <span
-            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
-                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite
-                    Application UI v2.0.0 <span
-                        className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Latest</span>
-                </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released
-                    on January 13th, 2022
-                </time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
-                    including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing
-                    pages.</p>
-                <a href="#"
-                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-                    <svg className="w-3.5 h-3.5 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
-                        <path
-                            d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
-                    </svg>
-                    Download ZIP</a>
-            </li>
-            <li className="mb-10 ml-6">
-        <span
-            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
-                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Figma v1.3.0</h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released
-                    on December 7th, 2021
-                </time>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components
-                    are first designed in Figma and we keep a parity between the two versions even as we update the
-                    project.</p>
-            </li>
-            <li className="ml-6">
-        <span
-            className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
-                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released
-                    on December 2nd, 2021
-                </time>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web
-                    components and interactive elements built on top of Tailwind CSS.</p>
-            </li>
-        </ol>
-
-        {/*<Chrono items={items} mode="VERTICAL" hideControls={true} cardHeight={100} cardWidth={300}*/}
-        {/*        theme={{*/}
-        {/*    primary: "white",*/}
-        {/*    secondary: "black",*/}
-        {/*    cardBgColor: "white",*/}
-        {/*    titleColor: "white",*/}
-        {/*    titleColorActive: "white",*/}
-        {/*}} />*/}
-    </div>
+    return (
+        <div className="text-white lg:w-[1024px] overflow-hidden pb-24 pl-8 pt-2">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+                {timelineData.map(({ title, timeframe, description }, index) => <TimelineItem title={title} timeframe={timeframe} description={description} />)}
+            </ol>
+        </div>
+    )
 }
