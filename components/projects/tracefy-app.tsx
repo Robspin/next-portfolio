3e0980eab39f2606d5a32a57fafb88f299ae207d
Robin Steeman <robinsteeman@Robins-MBP-M2.home>
2023-12-21 11:09:33 +0100

    progress

--- components/projects/tracefy-app.tsx
+++ components/projects/tracefy-app.tsx
@@ -25,31 +25,43 @@ export default function TracefyApp() {
                             <div className="mb-4 xl:mr-16">
                                 <h4 className="text-lg font-bold mb-2">Introduction</h4>
                                 <div className="mb-8">
-                                    One of my biggest projects while working at Tracefy was the creation of the new consumer app('s).<br/>
-                                    Our existing app was created by an external app development agency.
-                                    And because of our growth and desire flexibility and control the decision was made to create the new Tracefy app inhouse.<br/>
+                                    One of my biggest projects while working at Tracefy was the creation of the new
+                                    consumer app('s).<br/>
+                                    Tracefy is a Netherlands based company which anti theft solutions mainly focussed on
+                                    e-bikes.
+                                    They sell this tracker from local bicycle shops.
+                                    After purchase the customer can download and connect the Tracefy app.
+                                </div>
+                                <div className="mb-8">
+                                    The app can be used for several things, for example viewing the current bike location or viewing your past
+                                    trips.
+                                    But most importantly if the bicycle gets stolen. It can be used to activate
+                                    Tracefy's recovery service.
+                                    This means members of an authorized team will go out and recover your bike for you!
+                                </div>
+                                <h4 className="text-lg font-bold mb-2">Wait there's two?</h4>
+                                <div className="mb-8">
+                                    Far into the development of the Tracefy app was working on the release of a new product.
+                                    Namely a tracker called Tracefy Solo, a lighter version of Tracefy's main product (now called the Tracefy Pro).
+                                    This meant another lighter version app also needed to be built.
                                 </div>
                                 <h4 className="text-lg font-bold mb-2">Design</h4>
                                 <div className="mb-8">
-                                    Even though my ux/ui experience was lacking at the time. I was given the task of the design process.
+                                    Even though my ux/ui experience was lacking at the time. I was also given the task of the
+                                    design process.
                                     This was a lengthy process of doing customer research and collecting data.
-                                    Multiple design iterations and eventually starting over from scratch.
+                                    Multiple design iterations, a brief collaboration with an extarnal designer and starting over from scratch more than once.
                                 </div>
-                                <div>
-                                    The requirements were:
+                                <div className="mb-8">
+                                    In the end I am happy with the resulting clean and modern style.
                                 </div>
-                                <ul className="pl-4 list-disc mb-8">
-                                    <li>Clean and simple responsive design</li>
-                                    <li>Use transitions and small animations</li>
-                                    <li>Have a working contact form solution</li>
-                                    <li>Have a working good looking website fast</li>
-                                </ul>
                             </div>
                             <div>
-                                <div className="flex justify-end">
+                            <div className="flex justify-end">
                                     <div className="border border-green-600 mb-8 w-full">
-                                        <ImageDialog src="/assets/projects/tracefy-app/figma-1.png" alt="Tracefy App" >
-                                            <Image src="/assets/projects/tracefy-app/figma-1.png" alt="Tracefy App" height={400} width={1200} />
+                                        <ImageDialog src="/assets/projects/tracefy-app/figma-1.png" alt="Tracefy App">
+                                            <Image src="/assets/projects/tracefy-app/figma-1.png" alt="Tracefy App"
+                                                   height={400} width={1200}/>
                                         </ImageDialog>
                                         <div className="flex justify-center py-2 gap-4 border-t border-t-green-600 flex-wrap">
                                             {['expo', 'react-native', 'figma', 'typescript', 'reanimated', 'lottie'].map((tech, i) => <div key={i} className="px-2 py-1 text-xs bg-red-400 rounded-3xl font-medium tracking-wider">{tech}</div>)}
@@ -58,20 +70,20 @@ export default function TracefyApp() {
                                 </div>
                             </div>
                         </div>
-                        <h4 className="text-lg font-bold mb-2">Design</h4>
-                        <div className="mb-8 md:mr-14">
-                            After a few sketches I had an idea for which direction I wanted to go.<br/>
-                            For the color scheme I chose a light blue tint color that also worked in light mode.<br/>
-                            It was fun creating the solution 1 logo icons.
-                        </div>
                         <h4 className="text-lg font-bold mb-2">Development</h4>
-                        <div className="mb-8 md:mr-14">
-                            My framework of choice is React in combination with Nextjs<br/>
-                            Framer-motion was a interesting developer experience for implementing scroll based transitions and animations<br/>
-                            I will likely use the library again in the future.<br/>
-                            For the contact form I ended up using the shadcn components and used node-mailer as a backend solution for sending the emails<br/>
-                            In the end it took me a bit over a weekend to create.<br/>
+                        <div className="mb-8">
+                            As an experienced react developer I chose expo (react-native) as my framework for app development.
+                            We at Tracefy were low on developer capacity so a large part of the development of the Pro app was me working together with the creators of the Tracefy legacy app.
+                            Helpful with their app building experience they did not however have any react-native experience.
                         </div>
+                        {/*<h4 className="text-lg font-bold mb-2">Development</h4>*/}
+                        {/*<div className="mb-8 md:mr-14">*/}
+                        {/*    My framework of choice is React in combination with Nextjs<br/>*/}
+                        {/*    Framer-motion was a interesting developer experience for implementing scroll based transitions and animations<br/>*/}
+                        {/*    I will likely use the library again in the future.<br/>*/}
+                        {/*    For the contact form I ended up using the shadcn components and used node-mailer as a backend solution for sending the emails<br/>*/}
+                        {/*    In the end it took me a bit over a weekend to create.<br/>*/}
+                        {/*</div>*/}
                         <h4 className="text-lg font-bold mb-2">Links and information</h4>
                         <ul className="pl-4 list-disc mb-8">
                             <ExternalALink href="https://docs.expo.dev/">
