5d08f43664be57e56c70480c664ac0d14b445a7f
Robin Steeman <robinsteeman@Robins-MBP-M2.home>
2023-12-26 15:56:10 +0100

    carousel responsiveness upgrade

--- components/image-carousel.tsx
+++ components/image-carousel.tsx
@@ -14,23 +14,25 @@ type Props = { imageUrls: string[] }
 
 export default function ImageCarousel({ imageUrls }: Props) {
     return (
-        <div className="w-full lg:px-12 mb-24 mt-16 h-[400px] relative">
-            <Carousel className="left-0 right-0 bottom-0 top-0" opts={{ loop: true }} plugins={[Autoplay({ delay: 4000, stopOnInteraction: true, stopOnMouseEnter: true })]}>
-                <CarouselContent>
-                    {imageUrls.map((img, index) => (
-                        <CarouselItem key={index} className="cursor-pointer">
-                            <Card className="flex items-center justify-center">
-                                <CardContent className="aspect-square h-[200px] lg:h-[400px] relative">
-                                    <Image src={img} key={index} alt={`project image ${index}`} className="object-contain" fill />
-                                </CardContent>
-                            </Card>
-                        </CarouselItem>
-                        )
-                    )}
-                </CarouselContent>
-                <CarouselPrevious className="text-white" />
-                <CarouselNext className="text-white" />
-            </Carousel>
+        <div className="w-full lg:px-12 mb-24 mt-16 xl:h-[400px] relative">
+            <div className="left-0 right-0 bottom-0 top-0 max-lg:mx-8">
+                <Carousel className="" opts={{ loop: true }} plugins={[Autoplay({ delay: 4000, stopOnInteraction: true, stopOnMouseEnter: true })]}>
+                    <CarouselContent>
+                        {imageUrls.map((img, index) => (
+                            <CarouselItem key={index} className="cursor-pointer">
+                                <Card>
+                                    <CardContent className="h-[264px] md:h-[400px] relative">
+                                        <Image src={img} key={index} alt={`project image ${index}`} className="object-contain" fill />
+                                    </CardContent>
+                                </Card>
+                            </CarouselItem>
+                            )
+                        )}
+                    </CarouselContent>
+                    <CarouselPrevious className="text-white" />
+                    <CarouselNext className="text-white" />
+                </Carousel>
+            </div>
         </div>
     )
 }
