af8795945cb4cea1a1a27d637c6b287a68e46d72
Robspin <robin_steeman@hotmail.com>
2024-01-01 09:52:33 +0100

    added dancing alien

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -2,52 +2,37 @@ import PageContainer from "@/components/page-container"
 import { Canvas } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { useAnimations } from '@react-three/drei'
 import ContactForm from '@/components/contact-form'
 
+const Alien = ({ ...props }) => {
+    const group = useRef()
+    const { scene, animations } = useLoader(GLTFLoader, '/assets/gltf/dancing_alien/scene.gltf')
+    const { actions, mixer } = useAnimations(animations, group)
+
+    // model author attribution
+    // "Dancing Alien" (https://skfb.ly/oyVGw) by nthmn.exe is licensed under CC Attribution-NonCommercial-ShareAlike (http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+    useEffect(() => {
+        const action = actions['mixamo.com']
+        action?.play()
+    }, [mixer])
+
+    return <primitive ref={group} object={scene} {...props} />
+}
+
 const Astronaut = ({ ...props }) => {
     const group = useRef()
-    // const [reversed, setReversed] = useState(true)
-    const { scene, animations } = useLoader(GLTFLoader, '/assets/astronaut_dancing/scene.gltf')
+    const { scene, animations } = useLoader(GLTFLoader, '/assets/gltf/astronaut_dancing/scene.gltf')
     const { actions, mixer } = useAnimations(animations, group)
 
     // model author attribution
     // https://skfb.ly/oHDRW) by teknikfyndet is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
-    // const animateWithReverse = useCallback((reversed: boolean) => {
-    //     console.log('reversed: ', reversed)
-    //     const action = actions['Armature.001|DANCE']
-    //     // @ts-ignore
-    //     action.repetitions = 1
-    //     action.clampWhenFinished = true
-    //     if (reversed) {
-    //         action.timeScale = -1
-    //         action.paused = false
-    //     }
-    //     else {
-    //         action.timeScale = 1
-    //         action.paused = false
-    //     }
-    //     console.log(action)
-    //     // @ts-ignore
-    // }, [reversed])
-    //
-    // const callback = (e: any) => {
-    //     animateWithReverse(reversed)
-    //     setReversed(prev => !prev)
-    // }
-
     useEffect(() => {
-        // mixer.addEventListener('finished', callback)
         const action = actions['Armature.001|DANCE']
-        // @ts-ignore
-        // action.clampWhenFinished = true
-        // // @ts-ignore
-        // action.repetitions = 1
-        // @ts-ignore
-        action.play()
-        // return () => mixer.removeEventListener('finished', callback)
+        action?.play()
     }, [mixer])
 
     return <primitive ref={group} object={scene} {...props} />
@@ -61,7 +46,8 @@ const Contact = () => {
                     <Canvas>
                         <directionalLight intensity={0.75} />
                         <ambientLight intensity={0.25} />
-                        <Astronaut position={[0,-3.2,0]} rotation={[-0.4, 0, 0]} />
+                        <Astronaut position={[-2.5,-3.2,0]} rotation={[-0.4, 0, 0]} />
+                        <Alien position={[2.5, -2.7, 0]} scale={2.8} rotation={[-0.4, 0, 0]} />
                     </Canvas>
                 </div>
                 <div className="-mt-8 max-md:w-full max-sm:px-[60px] max-md:px-16">
